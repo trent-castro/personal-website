92fd329a76480cda9e3bafeb970f4ded49f30d3c
Trent Castro <keima.ayasaki@gmail.com>
2019-02-06 00:05:40 -0700

    Added initial hierarchy for video storage.

---
+++ scripts/musician.js
@@ -0,0 +1,16 @@
+var videoSeries = [
+    [
+        "Perfume",
+        ["Date", "Video Link", "Description"],
+        ["Date", "Video Link", "Description"],
+        ["Date", "Video Link", "Description"],
+        ["Date", "Video Link", "Description"],
+    ],
+    [
+        "[RWBY] Shine",
+        ["Date", "Video Link", "Description"],
+        ["Date", "Video Link", "Description"],
+        ["Date", "Video Link", "Description"],
+        ["Date", "Video Link", "Description"],
+    ],
+];
