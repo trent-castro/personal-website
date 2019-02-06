e22141052a1327da5c6fa85d70a47100e56dd615
Trent Castro <keima.ayasaki@gmail.com>
2019-02-05 23:16:21 -0700

    First push of current website.

---
+++ scripts/footerScript.js
@@ -0,0 +1,14 @@
+function populateFooter() {
+    var footerContent = document.getElementById("footerContent");
+    
+    footerContent.innerHTML =
+        '<p>' +
+        '&copy; Trent Castro 2019 ' +
+        ' - ' +
+        '<a href="https://www.linkedin.com/in/trentcastro/">LinkedIn</a>' +
+        ' - ' +
+        '<a href="mailto:castro.andrew.trent@gmail.com">castro.andrew.trent@gmail.com</a>' +
+        '</p>';
+}
+
+populateFooter();
---
+++ scripts/headerScript.js
@@ -0,0 +1,44 @@
+var headerContent = document.getElementById("headerContent");
+
+var links = [
+    ["index.html", "Home"],
+    ["games.html", "Games"],
+    ["musician.html", "Music"],
+    ["art.html", "Art"],
+    ["languages.html", "Languages"],
+    ["contact.html", "Contact"],
+    ["about.html", "About"], // Always the last
+];
+
+function populateHeader() {
+    populateNavigationBar();
+}
+
+function populateNavigationBar() {
+    // Open unordered list
+    var navigationBar = '<ul>';
+
+    for(x = 0; x < links.length; ++x) {
+        var link = links[x];
+
+        navigationBar += '<li';
+
+        // If the current page is the last page, place on the right side of the navigation bar
+        if(x == links.length - 1) {
+            navigationBar += ' style="float:right"';
+        }
+
+        // If the current page is being created, mark as an active page
+        if(window.location.href.indexOf(link[0]) > -1) {
+            navigationBar += ' class="activePage"';
+        }
+
+        // Creates a navigation node to the page
+        navigationBar += '><a href="' + link[0] + '">' + link[1] + '</a></li>';
+    }
+
+    navigationBar += '</ul>';
+    headerContent.innerHTML += navigationBar;
+}
+
+populateHeader();
